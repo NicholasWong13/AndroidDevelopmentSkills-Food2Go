ae2737d662e2945e1834ea6113eff8856569ff74
5009CEM-Quit-Smoking-App <nicholaswong613@gmail.com>
2022-11-23 17:54:41 +0800

    Commit By Nicholas Wong

--- app/src/main/java/com/example/food2go/Admin.java
+++ app/src/main/java/com/example/food2go/Admin.java
@@ -2,4 +2,46 @@ package com.example.food2go;
 
 public class Admin {
 
+    public String email;
+    public String password;
+    public String userName;
+
+    public Admin(){
+
+    }
+    public Admin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Admin(String email, String password, String userName) {
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
 }
